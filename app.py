6a60526bf0571f8911a192ed5c2b3857002ddd70
vignesh <vignesh2914@gmail.com>
2024-07-12 11:08:44 +0530

    API updated Scrap_job_data

--- app.py
+++ app.py
@@ -52,3 +52,36 @@ async def create_tables_api(config: DatabaseConfig):
         return {"status": "success", "message": "Tables created successfully", "result": result}
     except Exception as e:
         raise HTTPException(status_code=500, detail="Internal Server Error")
+
+
+@app.post("/scrape_job_data")
+async def scrape_job_data_api(config: JobDataConfig):
+    try:
+        job_data = scrape_job_data(config.job_keyword, config.location_keyword, config.time_limit)
+        
+        job_data_df = create_dataframe_of_job_data(job_data)
+        
+        save_job_data_dataframe_to_mysql(job_data_df)
+        
+        job_data_from_db = extract_job_data_from_DB()
+        
+        job_csv_file_path = save_job_data_to_csv(job_data_from_db)
+        
+        unique_job_df = get_unique_companies_df(job_data_df, "company_name")
+        
+        save_filtered_job_data_dataframe_to_mysql(unique_job_df)
+        
+        unique_job_data_from_db = extract_unique_job_data_from_db()
+        
+        unique_csv_file_path = save_unique_job_data_to_csv(unique_job_data_from_db)
+        
+        return {
+            "message": "Job data scraped and saved successfully",
+            "job_csv_file": job_csv_file_path.split('/')[-1],
+            "unique_csv_file": unique_csv_file_path.split('/')[-1],
+            "code": 200
+        }
+    except Exception as e:
+        traceback_str = ''.join(traceback.format_tb(e.__traceback__))
+        print(f"Exception: {e}\nTraceback: {traceback_str}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
